529cd8338ea31f34d3c4f9467fefd6fabfd75ef7
Melody Mastache <melodymastache@gmail.com>
2021-02-17 16:20:48 -0500

    nodemon implementation complete

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.static("public")); //exposing public directory to server
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`VanillaJS App listening at http://localhost:${PORT}`)
+});
